7dd2d77804095f5dbca4b37f284eac7e7fecc60a
mczerwi <marcinczrw@gmail.com>
2015-01-13 21:58:38 +0100

    Poprawione przetrzymywanie grafów w pamięci w teście wydajności.

--- src/test/java/pl/mczpk/gis/AlgorithmExecutionTest.java
+++ src/test/java/pl/mczpk/gis/AlgorithmExecutionTest.java
@@ -20,12 +20,12 @@ public class AlgorithmExecutionTest {
 	/**
 	 * Liczba iteracji testu
 	 */
-	private final static int TEST_ITERATIONS = 100;
+	private final static int TEST_ITERATIONS = 10;
 	
 	/**
 	 * Liczba wierzchołków w generowanych grafach
 	 */
-	private final static int NODES_IN_GENERATED_GRAPH = 500;
+	private final static int NODES_IN_GENERATED_GRAPH = 100;
 
 	private Random random = new Random();
 	private int minEdges = GraphUtils.getEdgesCountInTree(NODES_IN_GENERATED_GRAPH);
@@ -44,41 +44,42 @@ public class AlgorithmExecutionTest {
 	 */
 	@Test
 	public void testExecutionTimes() {
-		List<Graph> generatedGraphs = new ArrayList<Graph>();
-		
+		long bfsSearchTimeSum = 0;
+		long bfsFullTimeSum = 0;
+		long dfsSearchTimeSum = 0;
+		long dfsFullTimeSum = 0;
+
 		for(int i = 0; i < TEST_ITERATIONS; i++) {
 			int edgesToGenerate = minEdges + random.nextInt(maxEdges - minEdges) + 1;
-			generatedGraphs.add(RandomGraphFactory.getInstance().getGraph(NODES_IN_GENERATED_GRAPH, edgesToGenerate));
+			Graph graph = RandomGraphFactory.getInstance().getGraph(NODES_IN_GENERATED_GRAPH, edgesToGenerate);
+			
+			AlgorithmExecutionTime bfsExecutionTime = testForGraphSearch(new BreadthFirstSearch(), graph);
+			bfsSearchTimeSum += bfsExecutionTime.getSearchExecutionTime();
+			bfsFullTimeSum += bfsExecutionTime.getFullExecutionTime();
+			
+			AlgorithmExecutionTime dfsExecutionTime = testForGraphSearch(new DeepFirstSearch(), graph);
+			dfsSearchTimeSum += dfsExecutionTime.getSearchExecutionTime();
+			dfsFullTimeSum += dfsExecutionTime.getFullExecutionTime();
+			
+			System.out.println("Iteration " + (i + 1)  + " finished");
 		}
-		AlgorithmExecutionTime bfsExecutionTime = testForGraphSearch(new BreadthFirstSearch(), generatedGraphs);
-		AlgorithmExecutionTime dfsExecutionTime = testForGraphSearch(new DeepFirstSearch(), generatedGraphs);
 		
-		System.out.println("BFS mean search execution time: " + bfsExecutionTime.getSearchExecutionTime());
-		System.out.println("BFS mean full algorithm execution time: " + bfsExecutionTime.getFullExecutionTime());
-		System.out.println("DFS mean search execution time: " + dfsExecutionTime.getSearchExecutionTime());
-		System.out.println("DFS mean full algorithm execution time: " + dfsExecutionTime.getFullExecutionTime());
+		System.out.println("BFS mean search execution time: " + (double) bfsSearchTimeSum / TEST_ITERATIONS);
+		System.out.println("BFS mean full algorithm execution time: " + (double) bfsFullTimeSum / TEST_ITERATIONS);
+		System.out.println("DFS mean search execution time: " + (double) dfsSearchTimeSum / TEST_ITERATIONS);
+		System.out.println("DFS mean full algorithm execution time: " + (double) dfsFullTimeSum / TEST_ITERATIONS);
 	}
 	
-	private AlgorithmExecutionTime testForGraphSearch(GraphSearch search, List<Graph> generatedGraphs) {
-		long searchTimeSum = 0; 
-		long fullTimeSum = 0; 
+	private AlgorithmExecutionTime testForGraphSearch(GraphSearch search, Graph generatedGraph) {
 
-		long startTime;
-		long crTime;
-		for(int i=0;i<generatedGraphs.size(); ++i) {
-			GraphSearchResult searchResult = search.searchGraph(generatedGraphs.get(i), generatedGraphs.get(i).getRandomNode());
-			startTime = System.currentTimeMillis();
-			cr.restoreCycles(searchResult.getEdgesNotInTree());
-			crTime = System.currentTimeMillis() - startTime;
-			
-			searchTimeSum += searchResult.getExecutionTime();
-			fullTimeSum += searchResult.getExecutionTime() + crTime;
-
-			System.out.println("Iteration " + i + " done");
-		}
+		GraphSearchResult searchResult = search.searchGraph(generatedGraph, generatedGraph.getRandomNode());
+		long startTime = System.currentTimeMillis();
+		cr.restoreCycles(searchResult.getEdgesNotInTree());
+		long crTime = System.currentTimeMillis() - startTime;
 		
-		final double searchTime = (double) searchTimeSum / TEST_ITERATIONS;
-		final double fullTime = (double) fullTimeSum / TEST_ITERATIONS;
+		final long searchTime = searchResult.getExecutionTime();
+		final long fullTime = searchResult.getExecutionTime() + crTime;
+		
 		
 		return new AlgorithmExecutionTime() {
 			
